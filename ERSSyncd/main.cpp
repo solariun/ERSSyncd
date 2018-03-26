57ee21466e4af6a0743efcdd733c051625a8bd82
Apple <gustavocampos@gustavos-mac-mini.local>
2018-03-26 10:55:18 -0300

    Initial Commit

---
+++ ERSSyncd/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ERSSyncd
+//
+//  Created by GUSTAVO CAMPOS on 26/03/18.
+//  Copyright © 2018 GUSTAVO CAMPOS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
